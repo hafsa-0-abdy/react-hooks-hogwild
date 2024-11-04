88a1fa690acbdf3d13be23ab2e72b23ed608c88c
Habsa Abdirizack <habsa.abdirizack@student.moringaschool.com>
2024-11-04 21:05:16 +0300

    code challenge

---
+++ src/components /App.js
@@ -0,0 +1,33 @@
+import React,{useState} from "react";
+import Nav from "./Nav";
+import HogList from "./HogList"; 
+import hogs from "../porkers_data";
+
+
+function App() {
+	const [selectedHog, setSelectedHog] = useState(null);
+	const [filterGreased, setFilterGreased] = useState(false);
+	const [sortCriteria, setSortCriteria] = useState('name');
+	// To filter hogs based on greased status
+
+	const filteredHogs = filterGreased ? hogs.filter(hog => hog.greased) : hogs;
+
+	const sortedHogs = [...filteredHogs].sort((a, b) => {
+		if (sortCriteria === 'name') {
+			return a.name.localeCompare(b.name);
+		} else {
+			return a.weight - b.weight; // Sort by weight
+		}
+	});
+	return (
+		<div className="App">
+			<Nav setFilterGreased={setFilterGreased} setSortCriteria={setSortCriteria} />
+			<HogList hogs={sortedHogs} setSelectedHog={setSelectedHog} selectedHog={selectedHog}/>
+			
+		</div>
+		
+		
+	);
+}
+
+export default App;
